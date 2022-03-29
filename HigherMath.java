f9e114beb6dacc70adf34919af3b2ed753a53545
plabon <plabonjoseph@gmail.com>
2022-03-30 00:24:18 +0600

    solving higher math

--- HigherMath.java
+++ HigherMath.java
@@ -8,11 +8,20 @@ public class HigherMath {
         int caseNo = 1;
 
         while (caseNo <= cases) {
-            double a = Math.pow(in.nextInt(), 2);
-            double b = Math.pow(in.nextInt(), 2);
-            double c = Math.pow(in.nextInt(), 2);
+            double a = in.nextInt();
+            double b = in.nextInt();
+            double c = in.nextInt();
 
-            if ((a + b) == c) {
+            double result_c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+            boolean first_match = result_c == c;
+
+            double result_b = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
+            boolean second_match = result_b == b;
+
+            double result_a = Math.sqrt(Math.pow(c, 2) - Math.pow(b, 2));
+            boolean third_match = result_a == a;
+
+            if (first_match && second_match && third_match) {
                 System.out.println("Case " + caseNo + ": yes");
             } else {
                 System.out.println("Case " + caseNo + ": no");
