5b4d3ee7df0b924c9d4420f547289f9d2fb62ac4
plabon <plabonjoseph@gmail.com>
2022-03-30 17:10:53 +0600

    solved higher math

--- HigherMath.java
+++ HigherMath.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.lang.Math;
 
@@ -6,27 +7,25 @@ public class HigherMath {
         Scanner in = new Scanner(System.in);
         int cases = in.nextInt();
         int caseNo = 1;
+        double result;
+        int[] arr = new int[3];
 
         while (caseNo <= cases) {
-            double a = in.nextInt();
-            double b = in.nextInt();
-            double c = in.nextInt();
+            int i = 0;
+            while (i < 3) {
+                arr[i] = in.nextInt();
+                i++;
+            }
 
-            double result_c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-            boolean first_match = result_c == c;
+            Arrays.sort(arr);
 
-            double result_b = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
-            boolean second_match = result_b == b;
+            result = Math.sqrt(Math.pow(arr[0], 2) + Math.pow(arr[1], 2));
 
-            double result_a = Math.sqrt(Math.pow(c, 2) - Math.pow(b, 2));
-            boolean third_match = result_a == a;
-
-            if (first_match && second_match && third_match) {
+            if (result == arr[2]) {
                 System.out.println("Case " + caseNo + ": yes");
             } else {
                 System.out.println("Case " + caseNo + ": no");
             }
-
             caseNo++;
         }
     }
