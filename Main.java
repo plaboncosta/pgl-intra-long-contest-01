d83cec0f243d361f6212d4c2faf1bdd6ef512bf2
plabon <plabonjoseph@gmail.com>
2022-04-02 15:53:32 +0600

    the last digit number problem solved

--- Main.java
+++ Main.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
+
 // The Last Digit Problems
 public class Main {
+    public static int calculatePower(int a, int b) {
+        if (b == 0) return 1;
+        if (b % 2 == 0) {
+            int temporary = calculatePower(a, b / 2) % 10;
+            return (temporary * temporary);
+        }
+        return a * (calculatePower(a, b - 1) % 10);
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int c = in.nextInt();
         while (c-- > 0) {
             int a = in.nextInt();
             int b = in.nextInt();
-            a = a % 10;
-            b = (b % 4) + 4;
-            int r = (int) (Math.pow(a, b) % 10);
+            int r = calculatePower(a, b) % 10;
             System.out.println(r);
         }
     }
